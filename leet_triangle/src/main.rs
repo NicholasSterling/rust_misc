5f497ded68711858bd82a00b71319a4ba3abb2a9
Nicholas Sterling <Nicholas.Sterling@gmail.com>
2024-12-27 01:19:07 -0600

    More leet_triangle

--- leet_triangle/src/main.rs
+++ leet_triangle/src/main.rs
@@ -2,31 +2,44 @@
 // Calls the specified function with pair windows where the
 // second is mutable, e.g. for a,b,c,d it would call
 // f(&a, &mut b), f(&b, &mut c), f(&c, &mut d)
-fn pairs_mut<'a, Iter, T: 'a, Func>(mut xs: Iter, mut f: Func)
-    where Iter: Iterator<Item = &'a mut T>,
+
+fn pairs_mut<'a, Iter, T: 'a, Func>(xs: Iter, f: Func)
+    where Iter: IntoIterator<Item = &'a mut T>,
           Func: FnMut(&T, &mut T)
 {
-    if let Some(mut prev) = xs.next() {
-        for x in xs {
-            f(prev, x);
-            prev = x;
+    fn inner<'a, Iter, T: 'a, Func>(mut xs: Iter, mut f: Func)
+        where Iter: Iterator<Item = &'a mut T>,
+            Func: FnMut(&T, &mut T)
+    {
+        if let Some(mut prev) = xs.next() {
+            for x in xs {
+                f(prev, x);
+                prev = x;
+            }
         }
     }
+    inner(xs.into_iter(), f);
 }
 
 // Like pairs_mut(), but also folds an initial value through the pairs.
-fn fold_pairs_mut<'a, Iter, T: 'a, R, Func>(init: R, mut xs: Iter, mut f: Func) -> R
-where Iter: Iterator<Item = &'a mut T>,
+fn fold_pairs_mut<'a, Iter, T: 'a, R, Func>(init: R, xs: Iter, f: Func) -> R
+where Iter: IntoIterator<Item = &'a mut T>,
     Func: FnMut(R, &T, &mut T) -> R
 {
-    let mut acc = init;
-    if let Some(mut prev) = xs.next() {
-        for x in xs {
-            acc = f(acc, prev, x);
-            prev = x;
+    fn inner<'a, Iter, T: 'a, R, Func>(init: R, mut xs: Iter, mut f: Func) -> R
+    where Iter: Iterator<Item = &'a mut T>,
+        Func: FnMut(R, &T, &mut T) -> R
+    {
+        let mut acc = init;
+        if let Some(mut prev) = xs.next() {
+            for x in xs {
+                acc = f(acc, prev, x);
+                prev = x;
+            }
         }
+        acc
     }
-    acc
+    inner(init, xs.into_iter(), f)
 }
 
 #[cfg(test)]
@@ -36,14 +49,14 @@ mod tests {
     #[test]
     fn test_pairs_mut() {
         let mut v = vec!(1, 2, 3, 4);
-        pairs_mut(v.iter_mut(), |prev, this| *this += *prev);
+        pairs_mut(&mut v, |prev, this| *this += *prev);
         assert_eq!(v, vec!(1, 3, 6, 10));
     }
 
     #[test]
     fn test_fold_pairs_mut() {
         let mut v = vec!(1, 2, 3, 4);
-        let count = fold_pairs_mut(0, v.iter_mut(), |acc, prev, this| {
+        let count = fold_pairs_mut(0, &mut v, |acc, prev, this| {
             *this += *prev;
             acc + 1
         });
