c27281a679d99f4bc724927982a35deb9aacc4ee
Nicholas Sterling <Nicholas.Sterling@gmail.com>
2024-12-27 00:31:57 -0600

    More leet_triangle

--- leet_triangle/src/main.rs
+++ leet_triangle/src/main.rs
@@ -2,31 +2,29 @@
 // Calls the specified function with pair windows where the
 // second is mutable, e.g. for a,b,c,d it would call
 // f(&a, &mut b), f(&b, &mut c), f(&c, &mut d)
-fn pairs_mut<'a, Iter, T: 'a, Func>(xs: Iter, mut f: Func)
+fn pairs_mut<'a, Iter, T: 'a, Func>(mut xs: Iter, mut f: Func)
     where Iter: Iterator<Item = &'a mut T>,
           Func: FnMut(&T, &mut T)
 {
-    let mut prev = Option::<&T>::None;
-    for x in xs {
-        if let Some(prev) = prev {
+    if let Some(mut prev) = xs.next() {
+        for x in xs {
             f(prev, x);
+            prev = x;
         }
-        prev = Some(&*x);
     }
 }
 
 // Like pairs_mut(), but also folds an initial value through the pairs.
-fn fold_pairs_mut<'a, Iter, T: 'a, R, Func>(init: R, xs: Iter, mut f: Func) -> R
+fn fold_pairs_mut<'a, Iter, T: 'a, R, Func>(init: R, mut xs: Iter, mut f: Func) -> R
 where Iter: Iterator<Item = &'a mut T>,
     Func: FnMut(R, &T, &mut T) -> R
 {
-    let mut prev = Option::<&T>::None;
     let mut acc = init;
-    for x in xs {
-        if let Some(prev) = prev {
+    if let Some(mut prev) = xs.next() {
+        for x in xs {
             acc = f(acc, prev, x);
+            prev = x;
         }
-        prev = Some(&*x);
     }
     acc
 }
